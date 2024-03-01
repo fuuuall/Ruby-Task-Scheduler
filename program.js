29ca3ab7caae307d21ca29f93f4ad6913c8c845c
fuuuall <160235090+fuuuall@users.noreply.github.com>
2024-03-01 15:23:17 +0100

    Update program.js

--- program.js
+++ program.js
@@ -1,14 +1,56 @@
-# Your Ruby task scheduler code here
 require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler.new
+class TaskScheduler
+  def initialize
+    @scheduler = Rufus::Scheduler.new
+  end
 
-scheduler.every '1h' do
-  puts 'Do something every hour'
+  def schedule_every_hour
+    @scheduler.every '1h' do
+      begin
+        log('Do something every hour')
+        # Your hourly task logic here
+      rescue => e
+        log_error(e)
+      end
+    end
+  end
+
+  def schedule_at_specific_time(time, task)
+    @scheduler.at time do
+      begin
+        log("Do something at #{time}")
+        task.call
+      rescue => e
+        log_error(e)
+      end
+    end
+  end
+
+  def start
+    @scheduler.join
+  end
+
+  private
+
+  def log(message)
+    puts "[#{Time.now}] #{message}"
+  end
+
+  def log_error(error)
+    puts "[#{Time.now}] Error: #{error.message}"
+    puts error.backtrace.join("\n")
+  end
 end
 
-scheduler.at '2024/02/29 12:00:00' do
-  puts 'Do something at a specific time'
+# Usage example
+scheduler = TaskScheduler.new
+
+scheduler.schedule_every_hour
+
+scheduler.schedule_at_specific_time('2024/02/29 12:00:00') do
+  # Your specific time task logic here
 end
 
-scheduler.join
+scheduler.start
+
